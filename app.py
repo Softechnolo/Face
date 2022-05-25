d562c1939ba8f179114979722d04daf881af7f6c
Mohammed Audu <meetsofset@gmail.com>
2022-05-24 17:51:05 -0700

    24-05-2022

--- app.py
+++ app.py
@@ -4,34 +4,190 @@ import os
 import cv2
 import numpy as np
 import base64
+import datetime
+import io
+import pytesseract
+from PIL import Image
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField
+from wtforms.validators import InputRequired, Email, Length
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 # import our model from folder
 from face_recognition_and_liveness.face_liveness_detection.face_recognition_liveness_app import recognition_liveness
 
 app = Flask(__name__)
-app.secret_key = 'web_app_for_face_recognition_and_liveness'
+app.secret_key = 'web_app_for_face_recognition_and_liveness' # something super secret
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.sqlite'
+Bootstrap(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-UPLOAD_FOLDER = os.path.basename('uploads')
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class Users(db.Model):
-    username = db.Column(db.String(100), primary_key=True)
+
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100))
     name = db.Column(db.String(100))
     password = db.Column(db.String(100))
+    pass_photo = db.Column(db.String(100))
+    photo_id = db.Column(db.String(100))
+    staus = db.Column(db.String(100))
+    
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(int(user_id))
 
+class LoginForm(FlaskForm):
+    username = StringField('username', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=80)])
+    remember = BooleanField('remember me')
+
+
+class RegisterForm(FlaskForm):
+    name = StringField('Full Name', validators=[InputRequired(), Length(min=6, max=90)])
+    username = StringField('Email', validators=[InputRequired(), Length(min=4, max=20)])
+    password = PasswordField('password', validators=[InputRequired(), Length(min=5, max=80)])
+#Register AND LOGIN
 @app.route('/')
 def index():
+    return render_template('base.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        if user:
+            # compares the password hash in the db and the hash of the password typed in the form
+            if check_password_hash(user.password, form.password.data):
+                login_user(user, remember=form.remember.data)
+                return redirect(url_for('dashboard'))
+        return 'invalid username or password'
+
+    return render_template('login.html', form=form)
+
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        hashed_password = generate_password_hash(form.password.data, method='sha256')
+        # add the user form input which is form.'field'.data into the column which is 'field'
+        new_user = Users(username=form.username.data, name=form.name.data, password=hashed_password)
+        db.session.add(new_user)
+        db.session.commit()
+        return 'Account  has been created go and login!'
+
+    return render_template('signup.html', form=form)
+
+
+@app.route('/dashboard')
+@login_required
+def dashboard():
+    return render_template('dashboard.html', name=current_user.username)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+#Register and login Ends
+
+@app.route('/MainMenu')
+def MainMenu():
     return render_template('MainMenu.html')
 
+@app.route('/index1')
+def index1():
+    return render_template("index1.html")
 @app.route('/face_with_image')
 def face_with_image():
-    return render_template('index.html')
+    return render_template("index.html")
+# registation Star
+# t
+
+# registation Ends
 
 @app.route('/indivcam')
 def indivcam():
     return render_template('indivcam.html')
+
+@app.route('/scanner', methods=['GET', 'POST'])
+def scan_file():
+    if request.method == 'POST':
+        start_time = datetime.datetime.now()
+        f_name = request.form['f_name'].upper()
+        mid_name = request.form['mid_name'].upper()
+        l_name = request.form['l_name'].upper()
+        id_no = request.form['id_no'].upper()
+        image_data = request.files['file'].read()
+        file_to = request.files['file']
+        
+        f_name_status = ""
+        mid_name_status = ""
+        l_name_status = ""
+        id_no_status = ""
+        scanned_text = pytesseract.image_to_string(Image.open(io.BytesIO(image_data))).upper()
+        scanned_text2 = scanned_text.replace(" ", "").replace("\t", "")
+        
+        if f_name in scanned_text2:
+            f_name_status = "verified"
+        else:
+            f_name_status = "unverified"
+        if mid_name in scanned_text2:
+            mid_name_status = "verified"
+        else:
+            mid_name_status = "unverified"
+        if l_name in scanned_text2:
+            l_name_status = "verified"
+        else:
+            l_name_status = "unverified"
+        if id_no in scanned_text2:
+            id_no_status = "verified"
+        else:
+            id_no_status = "unverified"
+        
+
+        print("Found data:", scanned_text2)
+
+        session['data'] = {
+            "text": scanned_text2,
+            "time": str((datetime.datetime.now() - start_time).total_seconds()),
+            "fs" : f_name_status,
+            "ls": l_name_status,
+            "ids": id_no_status
+        }
+
+        return redirect(url_for('result'))
+
+
+@app.route('/result')
+def result():
+    if "data" in session:
+        data = session['data']
+        return render_template(
+            "result.html",
+            title="Result",
+            time=data["time"],
+            text=data["text"],
+            f = data["fs"],
+            l = data["ls"],
+            i = data["ids"],
+            words=len(data["text"].split(" "))
+        )
+    else:
+        return "Wrong request method."
+
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
@@ -86,7 +242,7 @@ def detect_faces(img):
 
     # Detect multiscale images (some images may be closer to camera than others)
     # result is a list of faces
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=5);
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.2, minNeighbors=10);
 
     # If not face detected, return empty list  
     if  len(faces) == 0:
@@ -111,22 +267,22 @@ def draw_rectangle(img, rect):
     cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 255), 2)
 
 
-@app.route('/login', methods=['GET','POST'])
-def login():
+@app.route('/login_page', methods=['GET','POST'])
+def login_page():
     if request.method == 'POST':
         session.pop('name', None)
         username = request.form['username']
         password = request.form['password']
-        user = Users.query.filter_by(username=username).first()
-        print(user)
-        if user is not None and user.password == password:
-            session['name'] = user.name # store variable in session
+        users = Users.query.filter_by(username=username).first()
+        print(users)
+        if users is not None and users.password == password:
+            session['name'] = users.name # store variable in session
             detected_name, label_name = recognition_liveness('face_recognition_and_liveness/face_liveness_detection/liveness.model',
                                                     'face_recognition_and_liveness/face_liveness_detection/label_encoder.pickle',
                                                     'face_recognition_and_liveness/face_liveness_detection/face_detector',
                                                     'face_recognition_and_liveness/face_recognition/encoded_faces.pickle',
                                                     confidence=0.5)
-            if user.name != detected_name and label_name == 'real':
+            if users.name != detected_name and label_name == 'real':
                 return redirect(url_for('main'))
             else:
                 return render_template('login_page.html', invalid_user=True, username=username)
@@ -145,6 +301,8 @@ def main():
     return render_template('main_page.html', name=name)
 
 if __name__ == '__main__':
+    pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+    #/usr/share/tesseract-ocr/4.00/tessdata
     db.create_all()
 
     # add users to database
